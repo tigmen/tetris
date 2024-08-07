339ccb169f6f95c18a08109d45273e0dc586d0a5
tigmen <tigmenal@gmail.com>
2024-08-08 01:41:19 +0300

    now blocks can be stopped by another, added 6 new blocks

--- tetris.cpp
+++ tetris.cpp
@@ -4,11 +4,21 @@
 #include <windows.h>
 #include <thread>
 #include <mutex>
+#include <vector>
+#include <random>
+#include <ctime>
 
-int speed = 30;
+int speed = 20; //*24 ms
 bool isLife = true;
 std::mutex locker;
 int map[8][16];
+int shape[][4][2] ={{{0,0},{1,0},{2,0},{-1,0}},
+                    {{0,0},{1,0},{-1,0},{1,1}},
+                    {{0,0},{1,0},{-1,0},{-1,1}},
+                    {{0,0},{1,0},{-1,0},{0,1}},
+                    {{0,0},{-1,0},{0,1},{1,1}},
+                    {{0,0},{1,0},{0,1},{-1,1}},
+                    {{0,0},{1,0},{1,1},{0,1}}};
 
 class Screen{
     public:
@@ -22,49 +32,60 @@ class Screen{
     {
         // Logic update
         locker.lock();
-        for(int i = 7;i >= 0; i--){
-             for(int j = 0;j < 16;j++){
-                if(map[i-1][j]==1 && map[i][j]==0 && i > 0) {
-                    std::cout << i << " " << j << std::endl;
-                    map[i][j] = speed;
-                    map[i-1][j] = 0;
-                }
-                if(map[i][j] == -1 && map[i-1][j] == 1){
-                    map[i-1][j] = -1;
-                    this->spawn();
-                }
-                else if(map[i][j] == 1 && i == 7){
-                    map[i][j] = -1;
-                    this->spawn();
-                }
-                else if(map[i][j]>1){
-                    map[i][j]--;
+        
+        std::vector<std::vector<int>> _temp;
+        bool _isGrounded = false;
+         for(int i = sizeof(map)/sizeof(map[0]) - 1; i >= 0; i--)
+             for(int j = 0;j < sizeof(map[0])/4;j++){
+                if(map[i][j] > 0){
+                    std::vector<int> _{i,j};
+                    _temp.push_back(_);
+                    if((map[i+1][j] < 0 || i == sizeof(map)/sizeof(map[0]) - 1) && map[i][j] == 1) 
+                        _isGrounded = true;
                 } 
             }
+        
+        for (int i = 0; i < _temp.size(); i++){
+            if(_isGrounded){
+                map[_temp[i][0]][_temp[i][1]] = -1;
+            }
+            else if(map[_temp[i][0]][_temp[i][1]] > 1) {
+                    map[_temp[i][0]][_temp[i][1]]--;
+            }
+            else{
+                map[_temp[i][0]][_temp[i][1]] = 0;
+                map[_temp[i][0]+1][_temp[i][1]] = speed;
+            }
         }
+        if(_isGrounded) this->spawn(rand() % 7);
 
         // Screen update
         system("cls");
-        std::string _t = "";
-        for(int i = 0;i < 8;i++){
-            for(int j = 0;j < 16;j++){
-                if(map[i][j]!=0) _t += "#";
+
+        std::string _t = "@@@@@@@@@@@@@@@@@@\n@@@ - TETRIS - @@@\n@@@@@@@@@@@@@@@@@@\n";
+        for(int i = 0;i < sizeof(map)/sizeof(map[0]);i++){
+            _t += "@";
+            for(int j = 0;j < sizeof(map[0])/sizeof(0);j++){
+                if(map[i][j] > 0) _t += "0";
+                else if(map[i][j] < 0) _t += "#";
                 else _t += ".";
             }
-            _t += "\n";
+            _t += "@\n";
         }
+        _t += "@@@@@@@@@@@@@@@@@@\n";
         locker.unlock();
         std::cout << _t << std::endl;
-        Sleep(10);
+        Sleep(24);
+    
+    }
         
-    }
 
-    void spawn() {
-        map[0][2] = speed; 
-        map[0][3] = speed;
-        map[0][4] = speed;
-        map[0][5] = speed;
+    void spawn(int param) {
+        for(int i = 0;i < 4;i++){
+            map[shape[param][i][1]][8+shape[param][i][0]] = speed;
+        }
     }
+    
 };
 class Controller{
     public:
@@ -74,8 +95,8 @@ class Controller{
             locker.lock();
             if(key == 75) {
                 
-                for(int i = 0;i < 8; i++){
-                    for(int j = 0;j < 15;j++)
+                for(int i = 0;i < sizeof(map)/sizeof(map[0]); i++){
+                    for(int j = 0;j < sizeof(map[0])/4 - 1;j++)
                         if(map[i][j+1]>0 && map[i][j]==0) {
                             map[i][j] = map[i][j+1];
                             map[i][j+1] = 0;
@@ -83,8 +104,8 @@ class Controller{
                 }
             }
             if(key == 77){
-                for(int i = 0;i < 8; i++){
-                    for(int j = 15;j > 0;j--)
+                for(int i = 0;i < sizeof(map)/sizeof(map[0]); i++){
+                    for(int j = sizeof(map[0])/4 - 1;j > 0;j--)
                         if(map[i][j-1]>0 && map[i][j]==0) {
                             map[i][j] = map[i][j-1];
                             map[i][j-1] = 0;
@@ -95,20 +116,23 @@ class Controller{
             locker.unlock();
         }
     }
-
-    private: 
-
 };
 
 int main() {
     system("color 05");
+
     Screen sc = Screen();
     Controller ct;
-    sc.spawn();
+
+    sc.spawn(rand() % 7);
+
     while(isLife){
-    std::thread scr([&]{sc.update();});
+    std::thread scr([&]{
+        srand(time(0));
+        sc.update();});
     scr.join();
     ct.control();
     }
+
     return 0;
 }
